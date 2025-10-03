ccf8e95d2987fca52f4ba5e57eb2e9e2ec69cf12
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-10-03 19:41:13 +0530

    feat: added array problem

---
+++ data_structures/array-and-hashing/is_equilibrium.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int findEquilibrium(vector<int> &arr) {
+        int n = arr.size();
+        long long totalSum = 0, leftSum = 0;
+        
+        for (int x : arr) totalSum += x;
+        
+        for (int i = 0; i < n; i++) {
+            totalSum -= arr[i]; 
+            
+            if (leftSum == totalSum) 
+                return i;
+            
+            leftSum += arr[i];
+        }
+        return -1;
+    }
+};
