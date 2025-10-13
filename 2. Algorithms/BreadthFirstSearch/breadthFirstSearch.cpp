b89b39a15791a3d564db3ddce194443301a71b06
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-10-13 11:39:30 +0530

    feat: breadth first search

---
+++ 2. Algorithms/BreadthFirstSearch/breadthFirstSearch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int V = adj.size();
+        vector<int> bfs;
+        vector<int> visited(V,0);
+        queue<int> q;
+        q.push(0);
+        visited[0] = 1;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(auto it: adj[node]){
+                if (!visited[it]) {
+                    visited[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
