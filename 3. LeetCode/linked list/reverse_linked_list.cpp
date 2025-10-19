81208dae6df29d81d9c6b67b5885be0f295a8308
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-10-19 09:50:16 +0530

    linked-list: updated

---
+++ 3. LeetCode/linked list/reverse_linked_list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    Node* reverseList(Node* head) {
+        Node *prev = NULL;
+        Node *curr = head;
+        Node* next;
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
