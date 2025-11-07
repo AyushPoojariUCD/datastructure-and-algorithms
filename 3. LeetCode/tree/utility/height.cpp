482cc8fa9073f31b21e20946ad79dfd749027e44
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-11-07 14:58:16 +0530

    tree: updated

---
+++ 3. LeetCode/tree/utility/height.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int val) {
+		data = val;
+		left = right = nullptr;
+	}
+};
+
+class BinaryTree {
+private:
+	Node* root;
+
+public:
+	BinaryTree() {
+		root = nullptr;
+	}
+
+	void insert(int val) {
+		Node* newNode = new Node(val);
+
+		if(!root) {
+			root = newNode;
+			return;
+		}
+
+		queue<Node*> q;
+		q.push(root);
+
+		while(!q.empty()) {
+			Node* node = q.front();
+			q.pop();
+			if(!node->left) {
+				node->left = newNode;
+				return;
+			} else {
+				q.push(node->left);
+			}
+			if(!node->right) {
+				node->right = newNode;
+				return;
+			} else {
+				q.push(node->right);
+			}
+		}
+	}
+    
+    int height(Node* node){
+        if(!node) return -1;
+        int lh = height(node->left);
+        int rh = height(node->right);
+        return 1 + max(lh,rh);
+    }
+    
+    int util(){
+        return height(root);
+    }
+};
+
+/*
+    Example: Tree
+    
+            [10]    - 0
+            / \
+        [20] [30]   - 1
+        / \
+    [40] [50]       - 2
+*/
+int main() {
+	BinaryTree tree;
+	tree.insert(10);
+	tree.insert(20);
+	tree.insert(30);
+	tree.insert(40);
+	tree.insert(50);
+	cout << "Height: " << tree.util();
+	return 0;
+}
